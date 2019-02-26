04ce688b45a97712006582ead72f71d7d443e3db
Andre Hijaouy <hijaouya@gmail.com>
2019-02-26 14:42:41 -0500

    change the way settings are organized

---
+++ server/settings/andre.py
@@ -0,0 +1,16 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': os.environ['PORT'],
+    }
+}
+
+# Activate Django-Heroku.
+django_heroku.settings(locals(), databases=False)
